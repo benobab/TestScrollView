9670eeaab96c6ef081ead325eae0017fca2ae79f
Ben <benlacroix83@gmail.com>
2016-01-02 15:17:07 +0100

    add custom Slider with transparency

--- TestScrollView/InitialViewController.swift
+++ TestScrollView/InitialViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class InitialViewController: UIViewController {
 
+    @IBOutlet weak var slider1: UISlider!
+    @IBOutlet weak var slider2: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.slider1.setThumbImage(UIImage(named: "Curseur"), forState: .Normal)
         // Do any additional setup after loading the view.
     }
 
